beb8af0c6efbe4ec34cac574489160724620d78e
timbergauge <timbergauge@gmx.us>
2016-08-19 23:19:51 -0400

    lemonbar

---
+++ scripts/lemonbar/bar.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#Graham's Lemonbar
+
+#this code ripped from the arch wiki
+Clock() {
+	DATETIME=$(date "+%a %b %d, %T")
+
+	echo -n "$DATETIME"
+}
+
+while true; do
+	echo "%{c}%{F#4D3A33}%{B#788C87} $(Clock) %{F-}%{B-}"
+	sleep 1
+done
+#end of arch wiki code
+
+
+
+#TIME=$(date "+%a %d %b %l:%M %p")
+
+#while :; do
+#
+#	echo -n "%{r}%$TIME"
+#
+#	sleep 1
+#done 
+
+#lemonbar -b -g "500x33x710+33" -B "#FFFFFF" -F "#000000"
